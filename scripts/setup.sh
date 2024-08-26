ab7378d1c1f2dc14fdf5f06089515ad512ab3441
Ant Weiss <anton@otomato.io>
2024-08-26 13:24:11 +0300

    Setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,3 +1,62 @@
-#! /bin/bash -xf
+#! /bin/bash -f
+
+sudo apt-get update
+
+#Install packages to allow apt to use a repository over HTTPS:
+sudo apt-get install -y apt-transport-https \
+	                ca-certificates \
+		        curl \
+		        software-properties-common \
+				jq \
+				conntrack \
+
+#Add Docker’s official GPG key:
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+	   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+	      $(lsb_release -cs) \
+	         stable"
+
+sudo apt-get update && sudo apt-get install -y docker-ce
+
+#install kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.18.0/bin/linux/amd64/kubectl
+
+#Make the kubectl binary executable.
+chmod +x ./kubectl
+
+#Move the binary in to your PATH.
+sudo mv ./kubectl /usr/bin/kubectl
+
+echo "source <(kubectl completion bash)" >> ~/.bashrc
+
+sudo usermod -aG docker $USER 
+
+sudo curl -s https://raw.githubusercontent.com/rancher/k3d/main/install.sh | TAG=v4.0.0 bash
+
+sudo k3d cluster create training -c cluster.yaml
+sudo mkdir $HOME/.kube
+sudo chown -R $USER $HOME/.kube
+k3d kubeconfig get training > $HOME/.kube/config
 
 
+#install kube-ps1
+cd ~/
+git clone https://github.com/jonmosco/kube-ps1.git
+echo 'source ~/kube-ps1/kube-ps1.sh' >> ~/.bashrc
+echo "PS1='[\u@\h \W \$(kube_ps1)]\$ '" >> ~/.bashrc
+cd -
+
+#install kubens and kubectx
+git clone https://github.com/ahmetb/kubectx.git ~/.kubectx
+COMPDIR=/usr/share/bash-completion/completions
+sudo ln -sf ~/.kubectx/completion/kubens.bash $COMPDIR/kubens
+sudo ln -sf ~/.kubectx/completion/kubectx.bash $COMPDIR/kubectx
+cat << FOE >> ~/.bashrc
+
+
+#kubectx and kubens
+export PATH=~/.kubectx:\$PATH
+FOE
